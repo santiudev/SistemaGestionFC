199a0827f7970d9f9721e6f018cd74cd75a658b4
Jose Lamensa <joselamensa10@gmail.com>
2024-09-25 13:59:49 -0300

    pruebo home

--- core/users/views.py
+++ core/users/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'users/home.html')
+    autos = {
+        'Auto1': {'modelo': 'Modelo1', 'marca': 'Marca1', 'año': 2020, 'vendedor': 'Vendedor1', 'estado': 'vendido'},  # Agregado estado
+        'Auto2': {'modelo': 'Modelo2', 'marca': 'Marca2', 'año': 2021, 'vendedor': 'Vendedor2', 'estado': 'disponible'},  # Agregado estado
+        # ... más autos ...
+    }
+
+    if request.method == 'POST':
+        for auto in autos.keys():
+            estado = request.POST.get(auto)
+            if estado:
+                autos[auto]['estado'] = estado  # Actualiza el estado del auto
+
+    return render(request, 'users/home.html', {'autos': autos})
