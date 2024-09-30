8c665a33262bea2632f4cc32758865d140bc0e92
Jose Lamensa <joselamensa10@gmail.com>
2024-09-30 19:41:24 -0300

    sistema

--- core/finanzas/urls.py
+++ core/finanzas/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import eliminar_medio_pago
+
 
 app_name = 'finanzas'
 urlpatterns = [
@@ -8,4 +10,5 @@ urlpatterns = [
     path('movimientos/crear/', views.crear_movimiento, name='crear_movimiento'),
     path('categorias/crear/', views.crear_categoria, name='crear_categoria'),
     path('mediosdepago/crear/', views.crear_mediopago, name='crear_mediodepago'),
+    path('mediopago/eliminar/<int:medio_pago_id>/', eliminar_medio_pago, name='eliminar_medio_pago'),
 ]
--- core/finanzas/views.py
+++ core/finanzas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404  # Añadir esta línea
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages  # Agregar esta línea para importar messages
 from .models import Movimiento, Categoria, MedioPago
 from .forms import MovimientoForm, CategoriaForm, MedioPagoForm
 
@@ -40,15 +41,25 @@ def crear_categoria(request):
             return redirect('finanzas:home')
     else:
         form = CategoriaForm()
-    return render(request, 'finanzas/crear_categoria.html', {'form': form})
-
+    categorias = Categoria.objects.all()
+    return render(request, 'finanzas/crear_categoria.html', {'form': form, 'categorias': categorias})
 
 def crear_mediopago(request):
-    if request.method == 'POST':
-        form = MedioPagoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('finanzas:home')
-    else:
-        form = CategoriaForm()
-    return render(request, 'finanzas/crear_mediodepago.html', {'form': form})
+       if request.method == 'POST':
+           form = MedioPagoForm(request.POST)
+           if form.is_valid():
+               form.save()
+               return redirect('finanzas:crear_mediodepago')
+           else:
+               # Agregar un mensaje de error
+               messages.error(request, 'Error al crear el medio de pago. Verifica los datos.')
+       else:
+           form = MedioPagoForm()
+       medios_pago = MedioPago.objects.all()
+       return render(request, 'finanzas/crear_mediodepago.html', {'form': form, 'medios_pago': medios_pago})
+
+def eliminar_medio_pago(request, medio_pago_id):
+    medio_pago = get_object_or_404(MedioPago, id=medio_pago_id)  # Obtener el medio de pago
+    medio_pago.delete()  # Eliminar el medio de pago
+    messages.success(request, 'Medio de pago eliminado con éxito.')  # Mensaje de éxito
+    return redirect('finanzas:crear_mediodepago.html')  # Redirigir a la vista deseada
