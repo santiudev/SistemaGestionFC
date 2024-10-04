7d2d48c0481af49507b320004f592a91eed0990f
Jose Lamensa <joselamensa10@gmail.com>
2024-10-04 11:43:45 -0300

    elimino boton eliminar cat

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/accounts/test/')),  # Redirige la raíz al nuevo destino
     path('accounts/', include('users.urls')),
     path('finanzas/', include('finanzas.urls')),
 ]
